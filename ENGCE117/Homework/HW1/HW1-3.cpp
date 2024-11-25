fd3f3683e23ded113257b5fedd660917eae7f7c7
MOOMIN <nichapha8895@gmail.com>
2024-11-25 14:52:54 +0700

    Add files via upload

---
+++ ENGCE117/Homework/HW1/HW1-3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    char f[ 10 ] ;
+    char l[ 10 ] ;
+
+    printf( "Frist Name: " ) ;
+    scanf( "%s", f ) ;
+
+    printf( "Last Name: " ) ;
+    scanf( "%s", l ) ;
+
+    printf( "%s %ss TC, RMUTL, Chiang Mai, Thailand", f, l ) ;
+
+    return 0 ;
+}//end function
